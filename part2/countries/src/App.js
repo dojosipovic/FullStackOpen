8a8caa97e26c8604c1e5afc1e3195c7ed02554d0
Dominik Josipovic <dominik.josipovic@gmail.com>
2023-08-19 17:21:38 +0200

    countries step1 2.18

---
+++ part2/countries/src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [countries, setCountries] = useState(null)
+  const [value, setValue] = useState('')
+  const [targets, setTargets] = useState(null)
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (!countries) {
+      console.log('fetching countries...')
+      axios
+        .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+        .then(response => {
+          setCountries(response.data)
+        })
+    }
+  }, [countries])
+
+  useEffect(() => {
+    if (targets === null) return
+    if (targets.length === 1) setCountry(targets[0])
+  }, [targets])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+    const value = event.target.value.toLowerCase()
+
+    if (value === '') {
+      setTargets(null)
+    } else {
+      const names = countries.map(c => c.name.common)
+      const targets = names.filter(c => c.toLowerCase().includes(value))
+      setTargets(countries.filter(c => c.name.common.toLowerCase().includes(value)))
+  
+      console.log(targets.length)
+    }
+  }
+
+  if (targets === null) return <div>find countries <input value={value} onChange={handleChange} /></div>
+  if (targets.length === 1 && country !== null) {
+    return(
+      <div>
+        find countries <input value={value} onChange={handleChange} />
+        <h2>{country.name.common}</h2>
+        <div>capital {country.capital[0]}</div>
+        <div>area {country.area}</div>
+        <p><strong>languages:</strong></p>
+        <ul>
+          {
+            Object.keys(country.languages)
+              .map(key => country.languages[key])
+              .map(l => <li key={l}>{l}</li>)
+          }
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt}/>
+      </div>
+    )
+  }
+  if (targets.length <= 10) {
+    return(
+      <div>
+        find countries <input value={value} onChange={handleChange} />
+        <div>{targets.map(c => <div key={c.name.common}>{c.name.common}</div>)}</div>
+      </div>
+    )
+  }
+  else if (targets.length > 10) {
+    return(
+      <div>
+        find countries <input value={value} onChange={handleChange} />
+        <div>Too many matches, specify another filter</div>
+      </div>
+    )
+  }
+}
+
+export default App
