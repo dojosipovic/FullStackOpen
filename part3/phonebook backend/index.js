6ac5b7b3b045a78350fe37dda951b04359b0738e
Dominik Josipovic <dominik.josipovic@gmail.com>
2023-08-20 20:33:09 +0200

    phonebook step8 3.8

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -2,18 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', { skip: (req, res) => req.method !== "POST"}))
@@ -102,8 +90,6 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
-
-app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
